cdcd6b8e69aee74bcb1761ea66474b1d912861e1
Nikich <nikich@zam.me>
2018-09-27 18:27:58 +0300

    feat: transactions filtering WAL-177

--- src/components/blocks/historyList/historyList.vue
+++ src/components/blocks/historyList/historyList.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="container-fluid h-100 py-4">
-    <div class="row h-100">
+  <div class="container-fluid h-100 py-4 position-relative">
+    <div :class="['row h-100', { 'blurred' : showFilters } ]">
       <div class="col-12 col-md-12 col-lg-6">
         <div class="tsx-wrap h-100">
           <div class="d-flex justify-content-between align-items-center">
             <h2 class="block-title">Transactions</h2>
-            <div v-if="false" class="d-flex">
-              <button type="button" class="btn btn-link p-0">
+            <div v-if="transactions.length > 0" class="d-flex">
+              <button type="button" class="btn btn-link p-0" @click="toggleFiltersVisibility">
                 Filter
                 <svg class="tsx__filter-icon">
                   <use xlink:href="#icon__filters"></use>
@@ -15,18 +15,18 @@
             </div>
           </div>
           <!--<div v-if="transactions.length > 0" class="tsx-search">-->
-            <!--<div class="d-flex">-->
-              <!--<input-->
-                <!--v-model="search"-->
-                <!--placeholder="Search"-->
-                <!--type="text"-->
-                <!--title="search transaction"-->
-                <!--class="w-100 tsx__search-input"-->
-              <!--&gt;-->
-            <!--</div>-->
+          <!--<div class="d-flex">-->
+          <!--<input-->
+          <!--v-model="search"-->
+          <!--placeholder="Search"-->
+          <!--type="text"-->
+          <!--title="search transaction"-->
+          <!--class="w-100 tsx__search-input"-->
+          <!--&gt;-->
+          <!--</div>-->
           <!--</div>-->
           <div
-            v-infinite-scroll="fetchHistory"
+            v-infinite-scroll="infiniteScrollCallback"
             :infinite-scroll-disabled="historyIsFetching"
             :infinite-scroll-distance="10"
             class="tsx-body mt-3"
@@ -150,11 +150,196 @@
         </div>
       </div>
     </div>
+    <transition appear name="fade">
+      <div v-show="showFilters" class="filters">
+        <button type="submit" class="filters__close-btn" @click="toggleFiltersVisibility">
+          <svg class="filters__close-icon">
+            <use xlink:href="#navbar__icon__close"></use>
+          </svg>
+        </button>
+        <div class="container-fluid filters__overlay h-100">
+          <div class="row justify-content-center h-100">
+            <div class="col-12 col-md-6 col-xl-5">
+              <div class="d-flex flex-column h-100">
+
+
+                <div class="filters__item">
+                  <h4 class="filters__item-header">Transaction Date</h4>
+                  <div class="d-flex mt-3">
+                    <div class="">
+                      <input
+                        v-model="formData.group"
+                        id="groupDay"
+                        class="d-none"
+                        type="radio"
+                        name="group"
+                        value="day"
+                      >
+                      <label
+                        for="groupDay"
+                        :class="['filters__tag', { 'filters__tag--active' : formData.group ===
+                        'day' }]"
+                      >
+                        Day
+                      </label>
+                    </div>
+                    <div class="ml-3">
+                      <input
+                        v-model="formData.group"
+                        id="groupWeek"
+                        class="d-none"
+                        type="radio"
+                        name="group"
+                        value="week"
+                      >
+                      <label
+                        for="groupWeek"
+                        :class="['filters__tag', { 'filters__tag--active' : formData.group ===
+                        'week' }]"
+                      >
+                        Week
+                      </label>
+                    </div>
+
+                    <div class="ml-3">
+                      <input
+                        v-model="formData.group"
+                        id="groupMonth"
+                        class="d-none"
+                        type="radio"
+                        name="group"
+                        value="month"
+                      >
+                      <label
+                        for="groupMonth"
+                        :class="['filters__tag', { 'filters__tag--active' : formData.group ===
+                        'month' }]"
+                      >
+                        Month
+                      </label>
+                    </div>
+                  </div>
+                </div>
+
+
+                <div class="filters__item">
+                  <h4 class="filters__item-header">Select dates</h4>
+                  <div class="d-flex mt-3">
+                    <input
+                      v-model="formData.from_time"
+                      type="text"
+                      class="filters__date-input"
+                      @input="formData.from_time = $event.target.value"
+                    >
+                    <input
+                      v-model="formData.until_time"
+                      type="text"
+                      class="filters__date-input ml-3"
+                      @input="formData.until_time = $event.target.value"
+                    >
+                  </div>
+                </div>
+
+
+                <div class="filters__item">
+                  <h4 class="filters__item-header">Operation</h4>
+                  <div class="d-flex mt-3">
+                    <div>
+                      <input
+                        v-model="formData.sent"
+                        class="d-none"
+                        type="checkbox"
+                        id="sent"
+                      >
+                      <label
+                        :class="['filters__tag', { 'filters__tag--active' : formData.sent }]"
+                        for="sent"
+                      >
+                        Sent
+                      </label>
+                    </div>
+                    <div class="ml-3">
+                      <input
+                        v-model="formData.received"
+                        class="d-none"
+                        type="checkbox"
+                        id="received"
+                      >
+                      <label
+                        :class="['filters__tag', { 'filters__tag--active' : formData.received }]"
+                        for="received"
+                      >
+                        Received
+                      </label>
+                    </div>
+                  </div>
+                </div>
+
+
+                <div class="filters__item">
+                  <h4 class="filters__item-header">Coins</h4>
+                  <div class="d-flex mt-3">
+                    <div>
+                      <input
+                        v-model="formData.coin"
+                        class="d-none"
+                        type="radio"
+                        name="coin"
+                        value="btc"
+                        id="btc"
+                      >
+                      <label
+                        :class="['filters__coin', { 'filters__coin--active' : formData.coin ===
+                        'btc' }]"
+                        for="btc"
+                      >
+                        <svg class="filters__coin-icon">
+                          <use xlink:href="#icon__bitcoin"></use>
+                        </svg>
+                        <span class="filters__coin-name">BTC</span>
+                      </label>
+                    </div>
+                    <div class="ml-3">
+                      <input
+                        v-model="formData.coin"
+                        class="d-none"
+                        type="radio"
+                        name="coin"
+                        value="bch"
+                        id="bch"
+                      >
+                      <label
+                        :class="['filters__coin', { 'filters__coin--active' : formData.coin ===
+                        'bch' }]"
+                        for="bch"
+                      >
+                        <svg class="filters__coin-icon">
+                          <use xlink:href="#icon__bitcoin-cash"></use>
+                        </svg>
+                        <span class="filters__coin-name">BCH</span>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+
+                <button
+                  type="button"
+                  class="btn filters__btn mt-auto mx-auto"
+                  @click="submitFilters"
+                >Done
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 import infiniteScroll from 'vue-infinite-scroll';
+import Inputmask from 'inputmask';
 import { format } from 'date-fns';
 
 import api from '@/api';
@@ -170,7 +355,16 @@ export default {
     return {
       page: 0,
       search: '',
+      showFilters: false,
       historyIsFetching: false,
+      formData: {
+        group: 'day',
+        from_time: '',
+        until_time: '',
+        sent: false,
+        received: false,
+        coin: null,
+      },
       transactions: [],
     };
   },
@@ -181,7 +375,11 @@ export default {
     historyItem,
   },
   methods: {
-    async fetchHistory() {
+    async fetchHistory({ params = {}, replace = true }) {
+      if (replace) {
+        this.page = 0;
+      }
+
       if (this.page === null) {
         return false;
       }
@@ -191,7 +389,11 @@ export default {
 
       const response = await api.wallet.transaction.getAll({
         params: {
-          group: 'day',
+          group: params.group,
+          from_time: params.from_time,
+          until_time: params.until_time,
+          direction: params.direction,
+          coin: params.coin,
           count: 38,
           page: this.page,
           timezone: date.getTimezoneOffset() / 60,
@@ -200,13 +402,47 @@ export default {
 
       if (response.data.result) {
         this.page = response.data.data.next;
-        this.transactions = [...this.transactions, ...response.data.data.transactions];
+        this.transactions = replace ?
+          [...response.data.data.transactions] :
+          [...this.transactions, ...response.data.data.transactions];
       } else {
         alert('Failed to fetch history'); // eslint-disable-line
       }
       this.historyIsFetching = false;
 
       return true;
+    },
+    async submitFilters() {
+      await this.fetchHistory({
+        params: {
+          group: this.formData.group,
+          from_time: this.fromTime,
+          until_time: this.untilTime,
+          direction: this.direction,
+          coin: this.formData.coin,
+        },
+        replace: true,
+      });
+
+      this.toggleFiltersVisibility();
+    },
+    async infiniteScrollCallback() {
+      await this.fetchHistory({
+        params: {
+          group: this.formData.group,
+          from_time: null,
+          until_time: null,
+          direction: null,
+          coin: null,
+        },
+        replace: false,
+      });
+    },
+    toggleFiltersVisibility() {
+      this.showFilters = !this.showFilters;
+
+      document.querySelector('.layout-content').classList.toggle('overflow-hidden');
+      document.querySelector('.layout__wallets-btn').classList.toggle('d-none');
     },
     parseGroupDate(unix) {
       return format(unix * 1000, 'DD MMM, dd');
@@ -217,6 +453,46 @@ export default {
         decimals: 2,
       });
     },
+  },
+  computed: {
+    fromTime() {
+      const [date, month, year] = this.formData.from_time.split('.');
+
+      if (Number.isNaN(+date) || Number.isNaN(+month) || Number.isNaN(+year)) {
+        return null;
+      }
+
+      return Math.floor(+new Date(Number(`20${year}`), month - 1, date) / 1000);
+    },
+    untilTime() {
+      const [date, month, year] = this.formData.until_time.split('.');
+
+      if (Number.isNaN(+date) || Number.isNaN(+month) || Number.isNaN(+year)) {
+        return null;
+      }
+
+      return Math.floor(+new Date(Number(`20${year}`), month - 1, date) / 1000);
+    },
+    direction() {
+      switch (true) {
+        case (this.formData.sent && this.formData.received):
+        case (!this.formData.sent && !this.formData.received):
+        default:
+          return null;
+        case (this.formData.sent):
+          return 'outgoing';
+        case (this.formData.received):
+          return 'incoming';
+      }
+    },
+  },
+  mounted() {
+    const im = new Inputmask('99.99.99', {
+      placeholder: 'DD.MM.YY',
+      showMaskOnHover: false,
+    });
+
+    im.mask(document.querySelectorAll('.filters__date-input'));
   },
 };
 </script>
@@ -240,7 +516,6 @@ export default {
 .tsx-body {
   height: calc(100% - 100px);
   overflow: auto;
-
   margin: 0 -16px;
   padding: 0 16px;
 
@@ -395,7 +670,6 @@ export default {
   right: 0;
   bottom: 0;
   top: 0;
-
   display: flex;
   align-items: center;
   justify-content: center;
@@ -407,5 +681,141 @@ export default {
   @include media-breakpoint-down(lg) {
     height: auto;
   }
+}
+
+.filters {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(11, 14, 67, 0.6);
+}
+
+.filters__overlay {
+  overflow-x: hidden;
+  overflow-y: visible;
+}
+
+.filters__item {
+  margin-top: 30px;
+}
+
+.filters__item-header {
+  font-size: 1.375rem;
+  font-weight: bold;
+  color: #ffffff;
+}
+
+.filters__tag {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 80px;
+  padding: 6px 20px;
+  margin-bottom: 0;
+  font-size: 1rem;
+  line-height: 1;
+  font-weight: 500;
+  color: #ffffff;
+  border: solid 1.3px $sky-blue;
+  border-radius: 24px;
+  transition: background-color .2s ease;
+  cursor: pointer;
+}
+
+.filters__tag--active {
+  background-color: $sky-blue;
+}
+
+.filters__date-input {
+  width: 160px;
+  height: 45px;
+  padding: 0 16px;
+  font-size: 1.125rem;
+  font-weight: 500;
+  color: #ffffff;
+  border: none;
+  border-radius: 8px;
+  background-color: rgba(255, 255, 255, 0.15);
+}
+
+.filters__coin {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 88px;
+  height: 88px;
+  border-radius: 16px;
+  background-color: #19215c;
+  transition: background-color .2s ease;
+  cursor: pointer;
+}
+
+.filters__coin--active {
+  background-color: $sky-blue;
+}
+
+.filters__coin-icon {
+  width: 34px;
+  height: 34px;
+}
+
+.filters__coin-name {
+  font-size: .75rem;
+  line-height: 1.17;
+  color: #ffffff;
+  margin-top: 12px;
+}
+
+.filters__btn {
+  display: flex;
+  flex-shrink: 0;
+  align-items: center;
+  justify-content: center;
+  width: 248px;
+  height: 74px;
+  margin-bottom: 10px;
+  font-size: 1.3125rem;
+  font-weight: bold;
+  letter-spacing: -0.7px;
+  color: #ffffff;
+  border-radius: 78px;
+  background-color: #8de37f;
+  box-shadow: 0 6px 14px -6px rgba(93, 118, 243, 0.24), 0 12px 24px 0 rgba(0, 0, 0, 0.06);
+  cursor: pointer;
+}
+
+.filters__close-btn {
+  position: absolute;
+  top: 36px;
+  right: 72px;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 2;
+
+  @include media-breakpoint-down(md) {
+    top: 31px;
+    right: 18px;
+  }
+}
+
+.filters__close-icon {
+  width: 24px;
+  height: 24px;
+
+  fill: white;
+
+  @include media-breakpoint-down(md) {
+    width: 20px;
+    height: 20px;
+  }
+}
+
+.blurred {
+  filter: blur(6px);
 }
 </style>
